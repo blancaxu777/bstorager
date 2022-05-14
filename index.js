27799bbc1d7fd03e4cfce2d0b2916f872cde0fe9
blanca <978059629@qq.com>
2022-05-15 02:15:51 +0800

    add clear func and change encrypt

--- index.js
+++ index.js
@@ -1,78 +1,101 @@
-import { encode, decode } from "js-base64";
+import Crypto, { AES } from 'crypto-js';
+const KEY = 'BLANCABLANCA';
 const timeTransformation = (time) => {
   if (time.length < 1) return 24 * 60 * 60 * 1000;
   const tt = time.substr(0, time.length - 1);
   if (!Number(tt)) return 24 * 60 * 60 * 1000;
-  if (time.endsWith("s")) {
+  if (time.endsWith('s')) {
     return tt * 1000;
-  } else if (time.endsWith("m")) {
+  } else if (time.endsWith('m')) {
     return tt * 60 * 1000;
-  } else if (time.endsWith("h")) {
+  } else if (time.endsWith('h')) {
     return tt * 60 * 60 * 1000;
-  } else if (time.endsWith("d")) {
+  } else if (time.endsWith('d')) {
     return tt * 24 * 60 * 60 * 1000;
-  } else if (time.endsWith("M")) {
+  } else if (time.endsWith('M')) {
     return tt * 30 * 24 * 60 * 60 * 1000;
-  } else if (time.endsWith("y")) {
+  } else if (time.endsWith('y')) {
     return tt * 365 * 24 * 60 * 60 * 1000;
   } else {
     return 24 * 60 * 60 * 1000;
   }
 };
-const save = (key, value, time = "1d") => {
+const save = (key, value, time = '1d') => {
   if (!key || !value) {
-    console.warn("bstorager: Please enter the parameters key and value!");
+    console.warn('bstorager: Please enter the parameters key and value!');
     return;
   }
   if (window.localStorage) {
-    const now = new Date();
     const tolValue = {
-      expireTime: now.getTime() + timeTransformation(time),
+      expireTime: Date.now() + timeTransformation(time),
       value,
     };
-    localStorage.setItem(encode(key), encode(encode(JSON.stringify(tolValue))));
+    localStorage.setItem(
+      'bs_' + key,
+      AES.encrypt(JSON.stringify(tolValue), KEY)
+    );
   } else {
-    console.warn("bstorager: Not supported at the moment!");
+    console.warn('bstorager: Not supported at the moment!');
   }
 };
 const get = (key) => {
   if (!key) {
-    console.warn("bstorager: Please enter the parameters key!");
+    console.warn('bstorager: Please enter the parameters key!');
     return;
   }
   if (window.localStorage) {
-    const tolKey = encode(key);
-    const isExist = localStorage.getItem(tolKey);
+    const isExist = localStorage.getItem('bs_' + key);
     if (!isExist) return null;
-    const now = new Date();
-    const tolValue = JSON.parse(decode(decode(isExist)));
-    if (tolValue.expireTime < now.getTime()) {
-      window.localStorage.removeItem(tolKey);
+    const tolValue = JSON.parse(
+      AES.decrypt(isExist, KEY).toString(Crypto.enc.Utf8)
+    );
+    if (tolValue.expireTime < Date.now()) {
+      window.localStorage.removeItem(key);
       return null;
     } else {
       return tolValue.value;
     }
   } else {
-    console.warn("bstorager: Not supported at the moment!");
+    console.warn('bstorager: Not supported at the moment!');
   }
 };
 const getExpire = (key) => {
   if (!key) {
-    console.warn("bstorager: Please enter the parameters key!");
+    console.warn('bstorager: Please enter the parameters key!');
     return;
   }
   if (window.localStorage) {
-    const tolKey = encode(key);
-    const isExist = localStorage.getItem(tolKey);
+    const isExist = localStorage.getItem('bs_' + key);
     if (!isExist) return null;
-    const tolValue = JSON.parse(decode(decode(isExist)));
-    return tolValue.expireTime
+    const tolValue = JSON.parse(
+      AES.decrypt(isExist, KEY).toString(Crypto.enc.Utf8)
+    );
+    if (tolValue.expireTime < Date.now()) {
+      window.localStorage.removeItem(key);
+      return null;
+    } else {
+      return tolValue.expireTime;
+    }
   } else {
-    console.warn("bstorager: Not supported at the moment!");
+    console.warn('bstorager: Not supported at the moment!');
   }
-}
+};
+const clear = () => {
+  for (let key in localStorage) {
+    if (key.startsWith('bs_')) {
+      const tolValue = JSON.parse(
+        AES.decrypt(localStorage.getItem(key), KEY).toString(Crypto.enc.Utf8)
+      );
+      if (tolValue.expireTime < Date.now()) {
+        window.localStorage.removeItem(key);
+        return null;
+      }
+    }
+  }
+};
 export default {
-  save,
   get,
+  save,
+  clear,
   getExpire,
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
-const path = require("path");
+const path = require('path');
 module.exports = {
-  mode: "production",
-  entry: "./index.js",
+  mode: 'production',
+  entry: './index.js',
   experiments: {
     outputModule: true,
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
     library: {
-      type: "module",
+      type: 'module',
     },
+    clean: true,
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: ['babel-loader'],
       },
     ],
   },
